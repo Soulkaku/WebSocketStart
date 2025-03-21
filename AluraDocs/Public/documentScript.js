c021916698d8c6819c33e1597a5d0f2562e0fd7e
tangogitlab@gmail.com <tangogitlab@gmail.com>
2025-03-21 17:30:39 -0300

    começa um evento e envia cada caractere que for pressionada e solta

--- AluraDocs/Public/documentScript.js
+++ AluraDocs/Public/documentScript.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+const campoTexto = document.getElementById("editor-texto");
+
+campoTexto.addEventListener("keyup", () => { //indentificar ao soltar uma tecla durante a digitação;
+    socket.emit("campo_texto", campoTexto.value); //começar um evento
+});

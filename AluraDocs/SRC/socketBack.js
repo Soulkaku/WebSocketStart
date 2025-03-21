d4a0a5ce6b08cbcbf327cd2670c6f058ada4466a
tangogitlab@gmail.com <tangogitlab@gmail.com>
2025-03-21 17:29:35 -0300

    recebendo texto do front-end

--- AluraDocs/SRC/socketBack.js
+++ AluraDocs/SRC/socketBack.js
@@ -2,4 +2,8 @@ import io from "./server.js";
 
 io.on("connection", (socket) => {
     console.log("Client connected from ID: ", socket.id);
+
+    socket.on("campo_texto", (texto) => {
+        console.log(texto);
+    })
 })

a725f9e33635eafd09483316dce02a1129e23478
tangogitlab@gmail.com <tangogitlab@gmail.com>
2025-03-21 16:35:47 -0300

    server funcionando

--- AluraDocs/SRC/server.js
+++ AluraDocs/SRC/server.js
@@ -7,8 +7,12 @@ const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-    app.use(express.static(path.join(__dirname, './AluraDocs', 'Public')));
-
+    
+    app.use(express.static(path.join(__dirname, '../Public')));
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../Public', 'index.html'));
+    });
+    
 app.listen(PORT, () => {
     console.log("Listen!");
 });

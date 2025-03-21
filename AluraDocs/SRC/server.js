28b415d33d8f448ec734bfa4cc1cd9f1717a1eb6
tangogitlab@gmail.com <tangogitlab@gmail.com>
2025-03-21 16:54:06 -0300

    socket integrado no front e server

--- AluraDocs/SRC/server.js
+++ AluraDocs/SRC/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { fileURLToPath } from "url";
+import { Server } from "socket.io";
+import http from "http";
+
 import path from "path";
 
 const app = express();
+const HTTPserver = http.createServer(app);
+    const io = new Server(HTTPserver);
 const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,7 +17,11 @@ const __dirname = path.dirname(__filename);
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../Public', 'index.html'));
     });
-    
-app.listen(PORT, () => {
+
+HTTPserver.listen(PORT, () => {
     console.log("Listen!");
+});
+
+io.on("connection", () => {
+    console.log("Client connected")
 });

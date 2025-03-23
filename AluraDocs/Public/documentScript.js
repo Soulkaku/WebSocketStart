45f51909fb0080c1bf96f1d8b52a910d1a3fcdb2
Thiago Gonçalves <tangogitlab@gmail.com>
2025-03-23 11:42:00 -0300

    o texto do client aparecerá para o outro

--- AluraDocs/Public/documentScript.js
+++ AluraDocs/Public/documentScript.js
@@ -5,3 +5,8 @@ const campoTexto = document.getElementById("editor-texto");
 campoTexto.addEventListener("keyup", () => { //indentificar ao soltar uma tecla durante a digitação;
     socket.emit("campo_texto", campoTexto.value); //começar um evento
 });
+
+socket.on("campo_textoClients", (texto) => {
+    //console.log(texto);
+    campoTexto.value = texto;
+});

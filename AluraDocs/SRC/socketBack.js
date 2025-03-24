0fe700f8ccbd3565942d4bab0ea908366f238f39
Thiago Gonçalves <tangogitlab@gmail.com>
2025-03-24 17:06:31 -0300

    recebe o nome da pagina e envia o texto somente para a pagina escolhida

--- AluraDocs/SRC/socketBack.js
+++ AluraDocs/SRC/socketBack.js
@@ -1,12 +1,18 @@
 import io from "./server.js";
 
 io.on("connection", (socket) => {
-    console.log("Client connected from ID: ", socket.id);
-
-    socket.on("campo_texto", (texto) => {
-        console.log(texto);
-        socket.broadcast.emit("campo_textoClients", texto); //send to all clients except you
+    socket.on("selecionar_documento", (nomeDocumento) => {
+       socket.join(nomeDocumento);
+       console.log(`Client ${socket.id} logged in ${nomeDocumento}`);
     });
+
+    socket.on("campo_texto", ({ texto, nomeDocumento }) => {
+        console.log(texto);
+        //socket.broadcast.emit("campo_texto", texto); //send to all clients
+        socket.to(nomeDocumento).emit("campo_texto", texto);
+    });
+
+
 
     socket.on("disconnect", (motivo) => {
         console.log(`O cliente ${socket.id} se desconectou, 

b974ab6343a54a099de13b9e3dc2d718d5f74180
Thiago Gonçalves <tangogitlab@gmail.com>
2025-03-25 17:37:46 -0300

    conexão feita com o banco

--- AluraDocs/SRC/server.js
+++ AluraDocs/SRC/server.js
@@ -2,8 +2,9 @@ import express from "express";
 import { fileURLToPath } from "url";
 import { Server } from "socket.io";
 import http from "http";
-
 import path from "path";
+import "dotenv/config";
+import connectToDataBase from "./config/dbconnect.js";
 
 const app = express();
 const HTTPserver = http.createServer(app);
@@ -12,11 +13,21 @@ const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-    
     app.use(express.static(path.join(__dirname, '../Public')));
 
-HTTPserver.listen(PORT, () => { //APP -> HTTPserver
-    console.log("Listen!");
-});
+
+
+async function connectToServer() {
+    connectToDataBase().then((connected) => {
+        console.log("CONEXÃO COM O BANCO DE DADOS E SERVER FEITAS COM ÊXITO!");
+
+        HTTPserver.listen(PORT, () => {
+            console.log("Listen!", PORT);
+        });
+        }).catch((erro) => {
+            console.error("ERRO AO CONECTAR AO BANCO DE DADOS: ", erro);
+    });
+}
+connectToServer();
 
 export default io;
